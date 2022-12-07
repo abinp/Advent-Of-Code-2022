2ec78e8ac73caba9db99be71dcafc96ca0da7987
Abin Punnoose <abin.punnoose@cadisme.com>
2022-12-07 09:36:28 +0300

    lint fixes

--- AoC-Day1.ts
+++ AoC-Day1.ts
@@ -1,42 +1,56 @@
 import * as fs from 'fs';
 
 async function maxCalories() {
-  const input  = fs.readFileSync('input.txt','utf8');
+  const input = fs.readFileSync('input.txt', 'utf8');
   const elfsByCalories = input.split('\n\n');
   let maxCalories = 0;
   let elfWithMaxCalories = -1;
   elfsByCalories.forEach((foodWithEachElf: string, index: number) => {
-    const calorieList = foodWithEachElf.split('\n').map((calorie: string) => Number(calorie));
-    const totalCalories = calorieList.reduce((total: number, currentCalorie: number) => currentCalorie + total, 0);
-    if(totalCalories > maxCalories) {
+    const calorieList = foodWithEachElf
+      .split('\n')
+      .map((calorie: string) => Number(calorie));
+    const totalCalories = calorieList.reduce(
+      (total: number, currentCalorie: number) => currentCalorie + total,
+      0
+    );
+    if (totalCalories > maxCalories) {
       maxCalories = totalCalories;
       elfWithMaxCalories = index;
     }
   });
-  console.log({ maxCalories, elfWithMaxCalories })
+  console.log({maxCalories, elfWithMaxCalories});
 }
 
 async function topThreeMaxCalories() {
-  const input  = fs.readFileSync('input.txt','utf8');
+  const input = fs.readFileSync('input.txt', 'utf8');
   const elfsByCalories = input.split('\n\n');
   let maxCalories1 = 0;
   let maxCalories2 = 0;
   let maxCalories3 = 0;
-  elfsByCalories.forEach((foodWithEachElf: string, index: number) => {
-    const calorieList = foodWithEachElf.split('\n').map((calorie: string) => Number(calorie));
-    const totalCalories = calorieList.reduce((total: number, currentCalorie: number) => currentCalorie + total, 0);
-    if(totalCalories > maxCalories1) {
+  elfsByCalories.forEach((foodWithEachElf: string) => {
+    const calorieList = foodWithEachElf
+      .split('\n')
+      .map((calorie: string) => Number(calorie));
+    const totalCalories = calorieList.reduce(
+      (total: number, currentCalorie: number) => currentCalorie + total,
+      0
+    );
+    if (totalCalories > maxCalories1) {
       maxCalories3 = maxCalories2;
       maxCalories2 = maxCalories1;
       maxCalories1 = totalCalories;
-    } else if(totalCalories > maxCalories2) {
+    } else if (totalCalories > maxCalories2) {
       maxCalories3 = maxCalories2;
       maxCalories2 = totalCalories;
-    } else if(totalCalories > maxCalories3) {
+    } else if (totalCalories > maxCalories3) {
       maxCalories3 = totalCalories;
-    } 
+    }
   });
-  console.log({ maxCalories1, maxCalories2, maxCalories3 }, maxCalories1 + maxCalories2 + maxCalories3);
+  console.log(
+    {maxCalories1, maxCalories2, maxCalories3},
+    maxCalories1 + maxCalories2 + maxCalories3
+  );
 }
 
+maxCalories();
 topThreeMaxCalories();
--- AoC-Day2.ts
+++ AoC-Day2.ts
@@ -1,69 +1,71 @@
 import * as fs from 'fs';
 
 function totalScore() {
-  const input  = fs.readFileSync('input.txt','utf8');
+  const input = fs.readFileSync('input.txt', 'utf8');
   const roundStrategyList = input.split('\n').map((round: string) => {
-    const [ opponent, player ] = round.split(' ');
-    return { opponent, player };
+    const [opponent, player] = round.split(' ');
+    return {opponent, player};
   });
   let totalScore = 0;
-  roundStrategyList.forEach((currentRound) => {
-    // const currentRound = roundStrategyList[0];
-    // currentRound
+  roundStrategyList.forEach(currentRound => {
     const currentRoundScore = updatedRoundScore(currentRound);
-    // console.log(currentRound, currentRoundScore)
     totalScore = totalScore + currentRoundScore;
   });
   console.log(totalScore);
 }
 
-function roundScore({ opponent, player }: {opponent: string, player: string }): number {
-  const dictionary = {
-    'X': 'Rock',
-    'Y': 'Paper',
-    'Z': 'Scissor',
-    'A': 'Rock',
-    'B': 'Paper',
-    'C': 'Scissor'
-  };
+function roundScore({
+  opponent,
+  player,
+}: {
+  opponent: string;
+  player: string;
+}): number {
   const resultMatrix = {
-    'X': { defaultScore: 1, win: 'C', lose: 'B', draw: 'A' },
-    'Y': { defaultScore: 2, win: 'A', lose: 'C', draw: 'B' },
-    'Z': { defaultScore: 3, win: 'B', lose: 'A', draw: 'C' }
+    X: {defaultScore: 1, win: 'C', lose: 'B', draw: 'A'},
+    Y: {defaultScore: 2, win: 'A', lose: 'C', draw: 'B'},
+    Z: {defaultScore: 3, win: 'B', lose: 'A', draw: 'C'},
   };
   let roundScore = resultMatrix[player].defaultScore;
-  if(opponent == resultMatrix[player].draw) {
+  if (opponent === resultMatrix[player].draw) {
     roundScore = roundScore + 3;
-  } else if(resultMatrix[player].win == opponent) {
+  } else if (resultMatrix[player].win === opponent) {
     roundScore = roundScore + 6;
   }
   return roundScore;
 }
 
-function updatedRoundScore({ opponent, player }: {opponent: string, player: string }): number {
-  const dictionary = {
-    'X': 'Rock',
-    'Y': 'Paper',
-    'Z': 'Scissor',
-    'A': 'Rock',
-    'B': 'Paper',
-    'C': 'Scissor'
-  };
+function updatedRoundScore({
+  opponent,
+  player,
+}: {
+  opponent: string;
+  player: string;
+}): number {
+  // const dictionary = {
+  //   X: 'Rock',
+  //   Y: 'Paper',
+  //   Z: 'Scissor',
+  //   A: 'Rock',
+  //   B: 'Paper',
+  //   C: 'Scissor',
+  // };
   const resultMatrix = {
-    'A': { playerShape: 'Rock', 'X': 'Paper', 'Y': 'Rock', 'Z': 'Scissor' },
-    'B': { playerShape: 'Paper', 'X': 'Scissor', 'Y': 'Paper', 'Z': 'Rock' },
-    'C': { playerShape: 'Scissor', 'X': 'Rock', 'Y': 'Scissor', 'Z': 'Paper' }
+    A: {playerShape: 'Rock', X: 'Paper', Y: 'Rock', Z: 'Scissor'},
+    B: {playerShape: 'Paper', X: 'Scissor', Y: 'Paper', Z: 'Rock'},
+    C: {playerShape: 'Scissor', X: 'Rock', Y: 'Scissor', Z: 'Paper'},
   };
-  const defaultScore = { 'Rock': 1, 'Paper': 2, 'Scissor': 3};
-  if(player == 'X') {
-    console.logdefaultScore[resultMatrix[opponent][player]];
+  const defaultScore = {Rock: 1, Paper: 2, Scissor: 3};
+  if (player === 'X') {
+    return defaultScore[resultMatrix[opponent][player]];
   }
-  if(player == 'Y') {
+  if (player === 'Y') {
     return 3 + defaultScore[resultMatrix[opponent][player]];
   }
-  if(player == 'Z') {
+  if (player === 'Z') {
     return 6 + defaultScore[resultMatrix[opponent][player]];
   }
+  return 0;
 }
 
 totalScore();
